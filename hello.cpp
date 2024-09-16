ca949d72b2c71f24d10786134815b6c39c9ecef8
e52354u <e52354u@BI-FDE-F-W136>
2024-09-16 16:18:26 +0200

    Ajoutez des fichiers projet.

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <HelloProjConfig.h>
+
+void hello_world(){
+	std::cout << "Hello World!\n";
+}
+
+void print_version(){
+	std::cout << "project version is "
+	<< HelloProj_VERSION_MAJOR
+	<< "."
+	<< HelloProj_VERSION_MINOR
+	<< std::endl;
+}
+
+int main(){
+	hello_world();
+	print_version();
+}
